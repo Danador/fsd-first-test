891d518162658c000918db85e75bb40b012085c5
Iliya <93042884+Danador@users.noreply.github.com>
2023-05-30 08:25:00 +0300

    изменил путь

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,8 @@
 import Routing from './index.vue';
 
 export const routes = [
-    {path: '/1', component: () => import('./authorization')},
-    {path: '/', component: () => import('./profile')},
+    {path: '/', component: () => import('./authorization')},
+    {path: '/profile', component: () => import('./profile')},
     {path: '/tasks', component: () => import('./tasks-list')},
     {path: '/tasks:id', component: () => import('./task-details')}
 ];
